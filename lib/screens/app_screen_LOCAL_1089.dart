5d071e6f23c12fd04a0cb3e9d48b00c4746e833a
bacon-king <jandreigalicia16@gmail.com>
2025-10-03 22:11:08 +0800

    "fixed branching issues"

---
+++ lib/screens/app_screen_LOCAL_1089.dart
@@ -0,0 +1,61 @@
+import 'package:application_prototype/screens/edit_profile_screen.dart';
+import 'package:application_prototype/screens/profile_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/app_state_provider.dart';
+import 'splash_screen.dart';
+import 'register_screen.dart';
+import 'login_screen.dart';
+import 'home_feed_screen.dart';
+import 'forums_screen.dart';
+import 'news_screen.dart';
+
+class AppScreen extends StatelessWidget {
+  const AppScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppStateProvider>(
+      builder: (context, appState, child) {
+        switch (appState.currentScreen) {
+          case 'register':
+            return RegisterScreen(
+              onBack: () => appState.goBack(),
+              onLogin: () => appState.navigateToScreen('login'),
+            );
+          case 'login':
+            return LoginScreen(
+              onBack: () => appState.goBack(),
+              onRegister: () => appState.navigateToScreen('register'),
+              onLoginSuccess: () => appState.navigateToScreen('home'),
+            );
+          case 'home':
+            return HomeFeedScreen(
+              onNavigate: (section) => appState.navigateToScreen(section),
+            );
+          case 'forum':
+            return ForumsScreen(
+              onNavigate: (section) => appState.navigateToScreen(section),
+            );
+          case 'feed':
+            return HIVRecommendationsScreen(
+              onNavigate: (section) => appState.navigateToScreen(section),
+            );
+          case 'profile':
+            return ProfileScreen(
+              onNavigate: (section) => appState.navigateToScreen(section),
+            );
+          case 'edit_profile':
+            return EditProfileScreen(
+              onNavigate: (section) => appState.navigateToScreen(section),
+            );
+          default:
+            return SplashScreen(
+              onRegister: () => appState.navigateToScreen('register'),
+              onLogin: () => appState.navigateToScreen('login'),
+            );
+        }
+      },
+    );
+  }
+}
